9acaab711c86ea595719e18e45e4c538bf6224a8
betterchen <wangchenphp@sina.com>
2018-11-24 16:46:12 +0800

    网站首页html完成

--- application/index/controller/Base.php
+++ application/index/controller/Base.php
@@ -8,23 +8,9 @@
 
 namespace app\index\controller;
 
-use think\App;
 use think\Controller;
-use think\Request;
 
 class Base extends Controller
 {
 
-    public $uid;
-    public function __construct(App $app = null)
-    {
-        parent::__construct($app);
-        $this->checkLogin();
-        $this->uid = session('user_id');
-    }
-
-    public function checkLogin(){
-        //seeion没有user_id 重新登录
-        if(!session('user_id')) $this->redirect('/index/login');
-    }
 }
--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -5,45 +5,6 @@ class Index extends Base
 {
     public function index()
     {
-        $userInfo = controller('user')->getUserInfo($this->uid);
-
-        //获取开奖信息
-        $award = controller('reward')->getAwardInfo(10);
-
-        $resultAttr = [];
-        $result = $award[1]['result'];
-        $resultLength = strlen($result);
-        for($i=0;$i<$resultLength;$i++){
-            array_push($resultAttr,$result{$i});
-        }
-
-        //获取商品信息
-        $goodsInfo = db('goods')
-            ->field('id,name,price,success_price,pic_url')
-            ->select();
-
-        //最近的购买
-        $orderInfo = db('order')
-            ->alias('o')
-            ->leftJoin('goods g','g.id = o.goods_id')
-            ->where('user_id', $this->uid)
-            ->field('o.goods_num,o.amount,o.created_date,g.name as goods_name')
-            ->order('o.id desc')
-            ->limit(5)
-            ->select();
-
-        $this->assign([
-            'name' => $userInfo['name'],
-            'gold' => $userInfo['gold']-$userInfo['frozen_gold'],
-            'user_photo' => $userInfo['photo'],
-            'lastTermNum' => $award[1]['term_num'],
-            'nowTermNum' => $award[0]['term_num'],
-            'result' => $resultAttr,
-            'goodsInfo' => $goodsInfo,
-            'orderInfo' => $orderInfo,
-            'award' => $award
-
-        ]);
         return $this->fetch();
     }
 
